673c320af4bc1bdb0834580597ec30108bb69647
Valérian Saliou <valerian@valeriansaliou.name>
2015-03-17 22:02:02 +0100

    Import

---
+++ README.rst
@@ -0,0 +1,19 @@
+===============
+waaave-gravatar
+===============
+
+`waaave-gravatar` makes it easy to add Gravatar support to your Django
+application through the addition of a template tag.
+
+
+*******
+History
+*******
+
+This code was taken from http://django-gravatar.googlecode.com, but it
+was superseded by the https://github.com/ericflo/django-avatar package,
+which provides a way more general approach to avatars in Django (not
+Gravatar-specific, with local Django caching).
+
+If, however, you want to stick to a Gravatar-only solution, this package
+may a bit more "lightweight" and easier to start using.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+ 
+setup(
+    name='waaave-gravatar',
+    version='0.5.1',
+    description='Gravatar Support in a Django Reusable Application',
+    author='Valerian Saliou',
+    author_email='valerian@valeriansaliou.name',
+    url='https://github.com/waaave/waaave-gravatar',
+    packages=[
+        'gravatar',
+        'gravatar.templatetags',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
