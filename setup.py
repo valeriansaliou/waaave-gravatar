9b0f15420ebbf83758de41b3c1a7e232ab9b313f
Valerian Saliou <valerian@valeriansaliou.name>
2016-07-10 22:11:51 +0200

    Bump to self namespace

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
- 
+
 setup(
     name='waaave-gravatar',
     version='0.5.1',
     description='Gravatar Support in a Django Reusable Application',
     author='Valerian Saliou',
     author_email='valerian@valeriansaliou.name',
-    url='https://github.com/waaave/waaave-gravatar',
+    url='https://github.com/valeriansaliou/waaave-gravatar',
     packages=[
         'gravatar',
         'gravatar.templatetags',
